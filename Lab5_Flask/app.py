cc9a84601f0c694d72a65a2dfe04e8c623db08ed
Kristina Vorobyeva <100286088+krimplessy@users.noreply.github.com>
2024-01-14 13:44:47 +1000

    Delete Lab5_Flask directory

--- Lab5_Flask/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# здесь должны импортироваться все программы-контроллеры, 
-# размещенные в папке controllers
-import controllers.index
-import controllers.hello
-import controllers.subject
